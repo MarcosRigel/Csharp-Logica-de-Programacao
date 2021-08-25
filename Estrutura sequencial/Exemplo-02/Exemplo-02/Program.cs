f9e52c6d837583c5815dc355676b7ba027116d5e
Marcos Rigel <marcosrigel99@gmail.com>
2021-08-25 17:11:58 -0300

    Exemplo 2

---
+++ Estrutura sequencial/Exemplo-02/Exemplo-02/Program.cs
@@ -0,0 +1,46 @@
+﻿/* Extremamente Básico
+ * Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de A e B atribuindo o seu resultado na
+ * variável X. Imprima X conforme exemplo apresentado abaixo. Não apresente mensagem alguma além daquilo que está sendo
+ * especificado e não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
+ * 
+ * Entrada
+ * A entrada contém 2 valores inteiros.
+ * 
+ * Saída
+ * Imprima a variável X conforme exemplo abaixo, com um espaço em branco antes e depois da igualdade. Obs: O X está em maiúsculo e deve ter um espaço
+ * antes e um espaço depois do sinal de igualdade.
+ * 
+ * ===============================================================================
+ * Exemplos de Entrada                              Exemplos de Saída
+ * ===============================================================================
+ * 10                                               X = 19
+ * 9
+ * ================================================================================
+ * -10                                              X = -6
+ * 4
+ * ================================================================================
+ * 15                                               X = 8
+ * -7
+ * 
+ */
+
+using System;
+
+namespace Exemplo_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, X;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+
+            X = A + B;
+
+            Console.WriteLine("X = " + X);
+            Console.ReadLine();
+        }
+    }
+}
