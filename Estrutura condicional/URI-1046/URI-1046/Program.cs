deee54fdfd4947d3e3820d175d4bf01d6e842732
Marcos Rigel <marcosrigel99@gmail.com>
2021-08-27 11:43:18 -0300

    Tempo de Jogo

---
+++ Estrutura condicional/URI-1046/URI-1046/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace URI_1046
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vetor = Console.ReadLine().Split(' ');
+
+            int horaInicial = int.Parse(vetor[0]);
+            int horaFinal = int.Parse(vetor[1]);
+            int horaToTal = 0;
+
+            if (horaInicial < horaFinal)
+            {
+                horaToTal = horaFinal - horaInicial;
+            }
+            else
+            {
+                horaToTal = 24 - horaInicial + horaFinal;
+            }
+
+            Console.WriteLine("O JOGO DUROU " + horaToTal + " HORA(S)");
+
+        }
+    }
+}
