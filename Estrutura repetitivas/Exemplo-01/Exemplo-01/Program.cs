c319494a972baec25a55718d7ef6c9892ea5c3b3
Marcos Rigel <marcosrigel99@gmail.com>
2021-08-27 22:29:19 -0300

    primeiro exemplo while

---
+++ Estrutura repetitivas/Exemplo-01/Exemplo-01/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Exemplo
+ * 
+ * Fazer um programa que lê números inteiros até que um zero seja lido. Ao final mostrar a soma
+ * dos números lidos.
+ * 
+ * Entrada                           Saída
+ * ==================================================
+ * 5                                 11   
+ * 2
+ * 4
+ * 0
+ * 
+ */
+
+using System;
+
+namespace Exemplo_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int soma = 0;
+
+            while(x != 0) {
+                soma = soma + x;
+                x = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(soma);
+
+        }
+    }
+}
