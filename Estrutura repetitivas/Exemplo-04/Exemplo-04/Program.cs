ca7fc81317e702de512277e83c5c5032d9f5dc06
Marcos Rigel <marcosrigel99@gmail.com>
2021-08-28 00:18:01 -0300

    primeiro exemplo com for

---
+++ Estrutura repetitivas/Exemplo-04/Exemplo-04/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Fazer um programa que lê um valor inteiro N e depois N números inteiros. Ao final, mostra
+ * a soma dos N números lidos
+ * 
+ * Exemplo
+ * ==========================================
+ * Entrada                  Saída
+ * ==========================================
+ * 3                        11
+ * 5
+ * 2
+ * 4
+ * 
+ */
+
+using System;
+
+namespace Exemplo_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, x, soma;
+
+            N = int.Parse(Console.ReadLine());
+
+            soma = 0;
+            for(int i = 1; i <= N; i++) {
+                x = int.Parse(Console.ReadLine());
+                soma = soma + x;
+            }
+
+            Console.WriteLine(soma);
+        }
+    }
+}
