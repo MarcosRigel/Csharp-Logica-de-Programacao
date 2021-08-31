841508bb2004663669b8c29dd42851d35d27346d
Marcos Rigel <marcosrigel99@gmail.com>
2021-08-31 09:03:16 -0300

    maior numero do vetor e a posicao do maior elemento

---
+++ vetores/Exercicio-01/Exercicio-01/Program.cs
@@ -0,0 +1,44 @@
+﻿/* Faça um programa que leia N números reais e armazene-os em um vetor. Em seguida, mostrar na tela o maior número
+ * do vetor (supor não haver empates). Mostrar também a posição do maior elemento.
+ * 
+ * Exemplo:
+ * =====================================================
+ * Entrada                              Saída
+ * =====================================================
+ * 6                                    14.0
+ * 8.0 4.0 10.0 14.0 13.0 7.0           3
+ */
+
+using System;
+using System.Globalization;
+
+namespace Exercicio_01 {
+    class Program {
+        static void Main(string[] args) {
+
+            int N = int.Parse(Console.ReadLine());
+            double[] vetor = new double[N];
+
+            string[] s = Console.ReadLine().Split();
+
+            for (int i = 0; i < N; i++) {
+                vetor[i] = double.Parse(s[i], CultureInfo.InvariantCulture);
+            }
+
+            double maior = 0.0;
+            double proximo = 0.0;
+            int posicao = 0;
+
+            for (int i = 0; i < N; i++) {
+                proximo = vetor[i];
+                if(proximo > maior) {
+                    maior = proximo;
+                    posicao = i;
+                }
+            }
+
+            Console.WriteLine(maior.ToString("F1", CultureInfo.InvariantCulture));
+            Console.WriteLine(posicao);
+        }
+    }
+}
