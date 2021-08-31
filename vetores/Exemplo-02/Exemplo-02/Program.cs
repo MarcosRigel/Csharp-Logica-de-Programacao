201ad85d4ccf0f45072da246a42a719792e180c9
Marcos Rigel <marcosrigel99@gmail.com>
2021-08-30 23:04:38 -0300

    numeros negativos

---
+++ vetores/Exemplo-02/Exemplo-02/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Faça um programa que leia N números inteiros e armazene-os em um vetor. Em seguida, mostrar na 
+ * tela todos os números negativos lidos.
+ * 
+ * Exemplo
+ * ======================================================
+ * Entrada                              Saída
+ * ======================================================
+ * 6                                    -2
+ * 8 -2 9 10 -3 -7                      -3
+ *                                      -7
+ */
+
+
+using System;
+
+namespace Exemplo_02 {
+    class Program {
+        static void Main(string[] args) {
+
+            int N = int.Parse(Console.ReadLine());
+            int[] vet = new int[N];
+
+            string[] s = Console.ReadLine().Split(' ');
+            for(int i=0; i<N; i++) {
+                vet[i] = int.Parse(s[i]);
+            }
+
+            for (int i = 0; i < N; i++) {
+                if(vet[i] < 0) {
+                    Console.WriteLine(vet[i]);
+                }
+            }
+        }
+    }
+}
