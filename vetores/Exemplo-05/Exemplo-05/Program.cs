f529410dce49f4e23b2427cf057f99cb228b7c2a
Marcos Rigel <marcosrigel99@gmail.com>
2021-08-31 08:36:37 -0300

    primeiro exemplo usando foreach

---
+++ vetores/Exemplo-05/Exemplo-05/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Fazer um programa para ler um valor N e depois o nome de N pessoas. Em seguida, mostrar os nomes
+ * lidos.
+ * 
+ * Exemplo:
+ * ====================================================
+ * Entrada                              Saída
+ * ====================================================
+ * 3                                    Nomes lidos:
+ * Maria                                Maria
+ * Joaquim                              Joaquim
+ * Ana                                  Ana
+ * 
+ */
+
+using System;
+
+namespace Exemplo_05 {
+    class Program {
+        static void Main(string[] args) {
+
+            int N = int.Parse(Console.ReadLine());
+            string[] vet = new string[N];
+
+            for (int i = 0; i < N; i++) {
+                vet[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("Nomes lidos:");
+            foreach (string x in vet) {
+                Console.WriteLine(x);
+            }
+
+        }
+    }
+}
