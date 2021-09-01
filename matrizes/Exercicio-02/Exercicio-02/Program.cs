0c77943a8f312a0ad01b80bcd03f0757bccb9841
Marcos Rigel <marcosrigel99@gmail.com>
2021-09-01 00:52:38 -0300

    soma dos elementos de cada linha

---
+++ matrizes/Exercicio-02/Exercicio-02/Program.cs
@@ -0,0 +1,48 @@
+﻿/*  Ler um número N e depois uma matriz quadrada NxN com números inteiros. Depois, mostrar na tela a soma dos
+ *  elementos de cada linha da matriz.
+ *  
+ *  Entrada:
+ *  A entrada contém o número N, depois os dados da matriz.
+ *  
+ *  Saída:
+ *  A saída contém os números representando a soma dos elementos de cada linha da matriz.
+ *  
+ *  Exemplo:
+ *  =====================================================
+ *  Entrada                                 Saída
+ *  =====================================================
+ *  3                                       11
+ *  5 2 4                                   19
+ *  10 3 6                                  29
+ *  9 8 12
+ * 
+ */
+
+using System;
+
+namespace Exercicio_02 {
+    class Program {
+        static void Main(string[] args) {
+
+            int N = int.Parse(Console.ReadLine());
+            int[,] matriz = new int[N, N];
+
+            for (int i = 0; i < N; i++) {
+                string[] s = Console.ReadLine().Split(' ');
+                for (int j = 0; j < N; j++) {
+                    matriz[i, j] = int.Parse(s[j]);
+                }
+            }
+
+            int soma;
+            for (int i = 0; i < N; i++) {
+                soma = 0;
+                for (int j = 0; j < N; j++) {
+                    soma = soma + matriz[i, j];
+                }
+                Console.WriteLine(soma);
+            }
+
+        }
+    }
+}
